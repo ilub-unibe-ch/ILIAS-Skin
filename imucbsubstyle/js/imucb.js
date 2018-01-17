0853059cce687b45de7137a2c67e253999e6f14b
Ioannis Glampedakis <ioannis.glampedakis@imu.unibe.ch>
2018-01-17 12:12:02 +0100

    fixed bug where the content of the dropdowns was not displayed

--- imucbsubstyle/js/imucb.js
+++ imucbsubstyle/js/imucb.js
@@ -25,25 +25,25 @@
     }
     if(left_nav) {
         var left_nav_lis = left_nav.find("li");
+        // the last kategorie/course in the navigation tree
         var course_lis = $("#left_nav div > div ul > li").has("a > img[alt*='Kurs']");
         if(!course_lis.length){
             course_lis = $("#left_nav div > div ul > li").has("a > img[alt*='Course']");
         }
         course_lis.css("margin-left", "0");
-        course_lis.not(":has('.ilHighlighted')").hide();
         var my_course_li = course_lis.has('.ilHighlighted').last();
         my_course_li.addClass("root");
+        // make all lis of the same branch visible in order to display the navigation menu.
         left_nav_lis.has('.root').css('display','block');
-        my_course_li.css('display','block');
-        my_course_li.find('ul li').css('display','block');
-        my_course_li.find("a").show();
+        // highlight the current course/module visible in the navigation menu
         var my_course_level_1_lis = my_course_li.children("ul").children("li");
         course_lis.find("li").has(".ilHighlighted").children('a').children('span').not(":empty").addClass("ilHighlighted");
-
+        // navigation menu: 3 entries per row as dropdown menus
         my_course_level_1_lis.addClass("col-sm-4");
         my_course_li.children("ul").addClass("row");
         my_course_level_1_lis.addClass("dropdownimucb");
 
+        // inner dropdown menus that show up only on hover
         var my_course_level_2plus_lis = my_course_level_1_lis.find("li");
         my_course_level_2plus_lis.addClass("dropdownimucbinner");
     }
@@ -78,6 +78,7 @@ $(window).load(function(){
 });
 
 
+// on hover loads asynchronously the subparts of the tree if they are not loaded
 $( document ).ajaxComplete(function() {
     var my_course_lis = $("#left_nav .dropdownimucb li");
     my_course_lis.each(function(){
