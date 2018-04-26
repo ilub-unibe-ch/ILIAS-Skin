b9f38bab7aa74e9ba4c3e5671355894d3edb2ee9
GiannisGl <ioannis.glampedakis@students.unibe.ch>
2018-04-26 11:15:52 +0200

    added comments in imucb.js

--- imucbsubstyle/js/imucb.js
+++ imucbsubstyle/js/imucb.js
@@ -1,5 +1,9 @@
 // hide or remove stuff from users
+// in this substyle it is needed to have only the necessary parts of ILIAS, without any possible distraction from the learning goal.
+// It leaves only breadcrumbs for navigation to the parent levels on non-folder levels (i.e. categories, courses, etc.)
 (function() {
+    // elements that only admins, authors and tutors have
+    // to differentiate between users and non-users
     var settings = $("[id*=settings]");
     var permissions = $("[id*=permissions]");
     var edit = $("[id*=edit]");
@@ -14,69 +18,63 @@
     if(!settings.length && !permissions.length && !edit.length && !presentation.length && !admin.length
         && !metadata.length && !presentation.length && !chapters.length && !style.length && !standperm.length
         && !map_areas.length){
+        // remove right rectangle 'action' that is not very helpful and distracting
         $(".il_ContainerListItem > .ilFloatRight").remove();
+        // as well as main menu navigation
         $(".ilMainMenu").hide();
-        current_folder = $("#left_nav div > div ul > li").has('.ilHighlighted').last().children("a").children("img[src*='fold']");
+        var current_folder = $("#left_nav div > div ul > li").has('.ilHighlighted').last().children("a").children("img[src*='fold']");
         $("#tab_info_short").hide();
         if(current_folder.length){
+            // remove the two default tabs of ilias that do not offer useful information for users.
+            // so that the tab line and therefore the page is clean.
             $("#tab_info, #tab_view_content").hide();
             $("#mainscrolldiv > ol.breadcrumb").hide();
         }
-        // current_course = $("#left_nav div > div ul > li").has("img[src*='crs']").has('.ilHighlighted').last().children("a").children("img[src*='crs']");
-        // if(current_course.length){
-        //         $("#mainscrolldiv > ol.breadcrumb").hide();
-        // }
     }
 })();
 
 
-// open left_nav, remove other course entries and float content to left
+// open left_nav, remove other course entries and float content to left.
+// left_nav is transformed in a horizontal navigation menu of the folders that the course/category contains.
 (function() {
     var left_nav = $("#left_nav");
+    // if left_nav is not open, opens it for further use.
     if(!left_nav.length){
         var imgtree = document.getElementById("imgtree");
         if(imgtree){
             imgtree.click();
         }
     }
+    // finds the current root, i.e. the course/category we are in, and add dropdowns for the next level courses/folders
     if(left_nav) {
+        // the last kategorie/course in the navigation tree that is highlighted is the root
         var left_nav_lis = left_nav.find("li");
-        // the last kategorie/course in the navigation tree
-        course_lis = $("#left_nav div > div ul > li").has('.ilHighlighted').has("a > img[src*='crs']"," a> img[src*='cat']");
+        var course_lis = $("#left_nav div > div ul > li").has('.ilHighlighted').has("a > img[src*='crs']"," a> img[src*='cat']");
         course_lis.css("margin-left", "0");
-        var my_course_li = course_lis.has('.ilHighlighted').last();
-        my_course_li.addClass("root");
-        // my_course_li.parent().siblings('a').css('display','block');
+        var root_li = course_lis.has('.ilHighlighted').last();
+        root_li.addClass("root");
         // make all lis of the same branch visible in order to display the navigation menu.
         left_nav_lis.has('.root').css('display','block');
         // highlight the current course/module visible in the navigation menu
-        var my_course_level_1_lis = my_course_li.children("ul").children("li");
+        var root_level_1_lis = root_li.children("ul").children("li");
+        // In left_nav menu, highlight also the top levels of the level were are in, so that it is visible where we are
         course_lis.find("li").has(".ilHighlighted").children('a').children('span').not(":empty").addClass("ilHighlighted");
         // navigation menu: 3 entries per row as dropdown menus
-        my_course_level_1_lis.addClass("col-sm-4");
-        my_course_li.children("ul").addClass("row");
-        my_course_level_1_lis.addClass("dropdownimucb");
+        root_level_1_lis.addClass("col-sm-4");
+        root_li.children("ul").addClass("row");
+        root_level_1_lis.addClass("dropdownimucb");
 
         // inner dropdown menus that show up only on hover
-        var my_course_level_2plus_lis = my_course_level_1_lis.find("li");
-        my_course_level_2plus_lis.addClass("dropdownimucbinner");
+        var root_level_2plus_lis = root_level_1_lis.find("li");
+        root_level_2plus_lis.addClass("dropdownimucbinner");
     }
 })();
 
 
 // on hover open js-trees
 $(window).load(function(){
-
-    // remove left_nav if we are not in a course folder and display breadcrumb)
-    my_course_level_1_lis = $("#left_nav div > div ul > li").has("a > img[src*='crs']", "a > img[src*='cat']").has('.ilHighlighted').last().children("ul").find("li");
-    if(!my_course_level_1_lis.length){
-        $("#left_nav_outer").remove();
-        $("#left_nav").remove();
-        $("#mainscrolldiv > ol.breadcrumb").show();
-    }
-
     // on hover simulate click to get the contents of the dropdownimucb if they are not there
-    my_course_level_1_lis.each(function(){
+    root_level_1_lis.each(function(){
         $(this).hover(
                 function(){
                     if($(this).hasClass("jstree-closed")) {
@@ -92,8 +90,8 @@ $(window).load(function(){
 
 // on hover loads asynchronously the subparts of the tree if they are not loaded
 $( document ).ajaxComplete(function() {
-    var my_course_lis = $("#left_nav .dropdownimucb li");
-    my_course_lis.each(function(){
+    var root_lis = $("#left_nav .dropdownimucb li");
+    root_lis.each(function(){
         $(this).hover(
             function(){
                 if($(this).hasClass("jstree-closed")) {
